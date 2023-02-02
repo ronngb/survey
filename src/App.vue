e08db6f853678975e38c6ab914b65a6e264bfc7b
ronngb <ronny.gabi@gmail.com>
2023-02-02 23:49:22 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form @submit.prevent="">
+      <h1>Survey</h1>
+      <h2>Q1: DELIVER THROUGH OTHERS</h2>
+      <!-- BUTTONS HERE -->
+      <div class="button-container">
+        <button
+          class="btn-choice"
+          @click="setAnswer('Q1', 'A')">
+          A. NOT EFFECTIVE
+        </button>
+        <button
+          class="btn-choice"
+          @click="setAnswer('Q1', 'B')">
+          B. NEITHER INEFFECTIVE OR EFFECTIVE
+        </button>
+        <button
+          class="btn-choice"
+          @click="setAnswer('Q1', 'C')">
+          C. EFFECTIVE
+        </button>
+      </div>
+
+      <h2>Q2: UNDERSTAND OTHERS PERSPECTIVES</h2>
+      <!-- BUTTONS HERE -->
+      <h2>Q3: SOLVE COMPLEX PROBLEMS</h2>
+      <!-- BUTTONS HERE -->
+
+      <!-- SUBMIT BUTTONS HERE -->
+      <button
+        type="submit"
+        @click="testFn()">
+        SUBMIT
+      </button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     HelloWorld,
   },
-};
+  data() {
+    return {
+      answersObj: { Q1: '', Q2: '', Q3: '' },
+    }
+  },
+  mounted() {
+    // console.log(this.answersObj.Q1)
+  },
+  updated() {
+    console.log(this.answersObj)
+  },
+  methods: {
+    setAnswer(prop, answer) {
+      this.answersObj[prop] = answer
+      console.log(this.answersObj)
+    },
+    testFn() {
+      console.log('HelloWorld')
+    },
+    testFn1() {
+      console.log(1)
+    },
+  },
+}
 </script>
 
 <style>
@@ -25,4 +80,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.button-container > button {
+  margin: 0 10px;
+  font-size: 20px;
+  background-color: white;
+  padding: 7px;
+  border: 1px solid black;
+  border-radius: 10px;
+}
+
+.btn-choice: ;
 </style>
